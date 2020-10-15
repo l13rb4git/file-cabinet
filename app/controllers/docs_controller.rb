9aba59e5c935ed98d98f9967d7fa955d5ec530a0
macunaima <gcgamer46@gmail.com>
2020-10-15 09:31:43 -0300

    before action to find a doc in show, edit, update and destroy actions

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,4 +1,6 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
   def index
     
   end
@@ -36,7 +38,7 @@ class DocsController < ApplicationController
   private
     
     def find_doc
-      
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
